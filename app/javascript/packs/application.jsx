3abb356c98fd60699031284325577f5fcf1c46cc
sc-arecrow <gsc928@gmail.com>
2020-01-03 21:04:29 +0800

    hosting on heroku changes

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -2,9 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../components/App";
